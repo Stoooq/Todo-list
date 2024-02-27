ac944ca1b34d5454f04c8f38230188c92110d5a8
Milosz Glowacki <milosz.glow@gmail.com>
2024-02-27 21:42:06 +0100

    scss and html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const burgerBtn = document.querySelector('.burger')
-const burgerBarsIco = document.querySelector('.fa-bars')
-const burgerXIco = document.querySelector('.fa-times')
+const burgerBarsIco = document.querySelector('.burger .fa-bars')
+const burgerXIco = document.querySelector('.burger .fa-times')
 const burgerUl = document.querySelector('.burger-container ul')
+const todoAddBtn = document.querySelector('.todo-add')
+const addInput = document.querySelector('.add-input')
+const confirmBtn = document.querySelector('.add-confirm')
+const cancelBtn = document.querySelector('.add-cancel')
+const addText = document.querySelector('.add-text')
+// const todoCheckBox = document.querySelector('.todo-check-box')
+// const todoCircleIco = document.querySelectorAll('.todo-check-box .fa-circle')
+// const todoCheckCircleIco = document.querySelectorAll('.todo-check-box .fa-circle-check')
 
 const handleBurger = () => {
     burgerBtn.classList.toggle('active')
@@ -10,4 +18,29 @@ const handleBurger = () => {
     burgerUl.classList.toggle('active')
 }
 
+const openAddInput = () => {
+    addInput.classList.remove('hide-add')
+    confirmBtn.addEventListener('click', confirmInput)
+    cancelBtn.addEventListener('click', cancelInput)
+}
+
+const confirmInput = () => {
+    
+}
+
+const cancelInput = () => {
+    addText.textContent = ''
+    addInput.classList.add('hide-add')
+}
+
+// const handleTodoCheck = (e) => {
+//     console.log(e.target);
+//     todoCircleIco.forEach((el) => el.classList.toggle('hide'))
+//     todoCheckCircleIco.forEach((el) => el.classList.toggle('hide'))
+//     todoCircleIco.classList.toggle('hide')
+//     todoCheckCircleIco.classList.toggle('hide')
+// }
+
 burgerBtn.addEventListener('click', handleBurger)
+todoAddBtn.addEventListener('click', openAddInput)
+// todoCheckBox.addEventListener('click', handleTodoCheck)
